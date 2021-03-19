f174ca54092fcc352aa3a09c5848487b9ae5eef2
mairalvinit <60435807+mairalvinit@users.noreply.github.com>
2021-03-20 00:38:50 +0530

    Level 4 : using bcrypt and 10 rounds of salting

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -35,16 +36,24 @@ app.get("/login",function(req,res){
 })
 app.post("/login", function(req,res){
     const username = req.body.username ;
-    const password = md5(req.body.password);
+    const password =req.body.password;
 
     User.findOne({email : username },function(err,foundUser){
         if(err){
             console.log(err);
         }else{
             if(foundUser){
-                if(foundUser.password === password ){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
+                    if(!err){
+                        if(result === true){
+                            res.render("secrets");
+                        }
+                    }
+                });
+                
+                    
+                
             }
         }
     })
@@ -55,17 +64,23 @@ app.get("/register",function(req,res){
     res.render("register");
 })
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email : req.body.username,
-        password : md5(req.body.password)
-    })
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email : req.body.username,
+            password : hash
+        })
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
+
+ 
 });
 
 
